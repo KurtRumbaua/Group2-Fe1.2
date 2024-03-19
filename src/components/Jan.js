bde51a621bbb316edead5a38777d9e35a226adb5
Jake <jan.kristoffer2002@gmail.com>
2024-03-19 11:41:46 +0800

    Added Jan.js and Janstyles.css

---
+++ src/components/Jan.js
@@ -0,0 +1,11 @@
+import './Janstyles.css'
+
+const Arjay = () => {
+    return (
+         <div className="container">
+             Jan Kristoffer Alminiana SN: 2021103819
+         </div>
+    )  
+};
+ 
+export default Jan;
